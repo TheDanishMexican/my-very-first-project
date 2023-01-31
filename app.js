110d74ba266bc483c50be0d098826370f0cb78b5
TheDanishMexican <danieljchristjansen@gmail.com>
2023-01-31 10:17:52 +0100

    De første ændringer
    Lidt introduktion

---
+++ app.js
@@ -0,0 +1,5 @@
+document.body.innerHTML += "Hello JavaScript!";
+//alert("Hej Asbjørn.js");
+console.log("Hej Asbjørn.js");
+console.log("Fungerer det faktisk.js");
+console.log("Ja det gør det da");
