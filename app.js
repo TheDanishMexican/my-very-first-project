f2e4b192be76ab92b187ac8941a50bc0f2386037
TheDanishMexican <danieljchristjansen@gmail.com>
2023-01-31 12:41:01 +0100

    Hello JavaScript change
    Assignment

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "";
 //alert("Hej Asbjørn.js");
 console.log("Hej Asbjørn.js");
 console.log("Fungerer det faktisk.js");
 console.log("Ja det gør det da");
+function HelloDaniel() {
+  document.querySelector("h3").textContent =
+    "Great success " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", HelloDaniel);
+document.querySelector("input").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector("button").click();
+  }
+});
